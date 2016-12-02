2dd7f7c8a5b5c9715d345363ca04067a72cbc678
William Kretschmer <kretsch@mit.edu>
2016-12-02 13:20:22 -0500

    New Circle.intersections implementation
    Changes the specification so now it returns the intersection points as angles with respect to both centers.
    
    Also imported FixedPoint._ to make code a bit cleaner

--- src/circlepuzzles/math/Circle.scala
+++ src/circlepuzzles/math/Circle.scala
@@ -1,5 +1,7 @@
 package circlepuzzles.math
 
+import FixedPoint._
+
 /**
   * Immutable circles on the Euclidean plane.
   * @param x X-coordinate of this circle's center.
@@ -22,7 +24,7 @@ case class Circle(x: FixedPoint, y: FixedPoint, radius: FixedPoint) {
     val dy = that.y - y
     // Distance between centers
     val distSquared = dx.pow(2) + dy.pow(2)
-    val dist = FixedPoint.sqrt(distSquared)
+    val dist = sqrt(distSquared)
     if(dist >= radius + that.radius) { // Circles don't intersect or are tangent at one point
       UnitArcs.Empty
     }
@@ -36,49 +38,70 @@ case class Circle(x: FixedPoint, y: FixedPoint, radius: FixedPoint) {
     }
     else {
       // The cosine of half of the angle that spans the intersection; follows from law of cosines
-      val cosHalfTheta = (distSquared + radius.pow(2) - that.radius.pow(2)) / (FixedPoint.Two * radius * dist)
-      val halfTheta = FixedPoint.acos(cosHalfTheta).abs
+      val cosHalfTheta = (distSquared + radius.pow(2) - that.radius.pow(2)) / (Two * radius * dist)
+      val halfTheta = acos(cosHalfTheta).abs
       // Compute the angle to the center of the other circle
-      val angleToCenter = FixedPoint.atan2(dy, dx)
+      val angleToCenter = atan2(dy, dx)
       // The arc runs between angleToCenter +- halfTheta
-      UnitArcs(angleToCenter - halfTheta, halfTheta * FixedPoint.Two)
+      // UnitArcs takes care of computing this mod 2*pi
+      UnitArcs(angleToCenter - halfTheta, halfTheta * Two)
     }
   }
 
   /**
     * Compute the set of nondegenerate intersections between the boundaries of this circle and the other circle. This is
-    * to say that if the circles do not intersect or are equal, this returns the empty set. The result is expressed as a
-    * set of angles with respect to the center of this circle.
+    * to say that if the circles do not intersect or are equal, this returns the empty set.
     * @param that Circle whose boundary is intersected with the boundary of this circle.
-    * @return A set containing all angles with respect to this circle's center at which the boundary of this circle
-    * nondegenerately intersects the boundary of the argument. The returned angles are in the range [0,2*pi).
+    * @return A set of pairs `(thisAngle, thatAngle)` indicating that the point on the boundary of `this` at `thisAngle`
+    * from `this` circle's center intersects the boundary of the other circle, where `thatAngle` is the angle from the
+    * center of `that` to the point of intersection. The returned angles are in the range [0,2*pi).
     */
-  def intersections(that: Circle): Set[FixedPoint] = {
+  def intersections(that: Circle): Set[(FixedPoint, FixedPoint)] = {
     // TODO: it would be nice to remove some of the duplicate code between this and intersectionArc
     // Distances between x and y coordinates
     val dx = that.x - x
     val dy = that.y - y
     // Distance between centers
     val distSquared = dx.pow(2) + dy.pow(2)
-    val dist = FixedPoint.sqrt(distSquared)
+    val dist = sqrt(distSquared)
     // Compare the distance between centers to the sum and difference of radii
     val distToSum = dist.compare(radius + that.radius)
     val distToDiff = dist.compare((radius - that.radius).abs)
     if(distToSum > 0 || distToDiff < 0 || that == this) { // Circles don't intersect or are equal
       Set.empty
     }
-    else if(distToSum == 0 || distToDiff == 0) { // Circles intersect at one point
+    else if(distToSum == 0) { // Circles separate and intersect at one point
       // The point of intersection is at the angle between the centers
-      Set(FixedPoint.mod2Pi(FixedPoint.atan2(dy, dx)))
+      val thisAngle = mod2Pi(atan2(dy, dx))
+      // The angle from the other center differs from thisAngle by pi; they are opposite angles
+      val thatAngle = mod2Pi(thisAngle + Pi)
+      Set((thisAngle, thatAngle))
+    }
+    else if(distToDiff == 0) { // Smaller circle contained in larger circle and circles intersect at one point
+      // The angle is the same from both centers
+      // This angle is also the angle from the center of the larger to the smaller circle
+      val angle = if(radius > that.radius) mod2Pi(atan2(dy, dx)) else mod2Pi(atan2(-dy, -dx))
+      Set((angle, angle))
     }
     else{
-      // The cosine of half of the angle that spans the intersection; follows from law of cosines
-      val cosHalfTheta = (distSquared + radius.pow(2) - that.radius.pow(2)) / (FixedPoint.Two * radius * dist)
-      val halfTheta = FixedPoint.acos(cosHalfTheta).abs
-      // Compute the angle to the center of the other circle
-      val angleToCenter = FixedPoint.atan2(dy, dx)
-      // The intersections are angleToCenter +- halfTheta
-      Set(FixedPoint.mod2Pi(angleToCenter - halfTheta), FixedPoint.mod2Pi(angleToCenter + halfTheta))
+      val radiusSquared = radius.pow(2)
+      // See http://paulbourke.net/geometry/circlesphere/
+      val a = (distSquared + radiusSquared - that.radius.pow(2)) / (Two * dist)
+      val h = sqrt(radiusSquared - a.pow(2))
+      // (midX, midY) is the intersection of the line between the centers and the line between the circle intersections
+      val midX = x + a * dx / dist
+      val midY = y + a * dy / dist
+
+      val newX1 = midX + h * dy / dist
+      val newY1 = midY - h * dx / dist
+      val thisAngle1 = mod2Pi(atan2(newY1 - y, newX1 - x))
+      val thatAngle1 = mod2Pi(atan2(newY1 - that.y, newX1 - that.x))
+
+      val newX2 = midX - h * dy / dist
+      val newY2 = midY + h * dx / dist
+      val thisAngle2 = mod2Pi(atan2(newY2 - y, newX2 - x))
+      val thatAngle2 = mod2Pi(atan2(newY2 - that.y, newX2 - that.x))
+      Set((thisAngle1, thatAngle1), (thisAngle2, thatAngle2))
     }
   }
 
@@ -92,10 +115,10 @@ case class Circle(x: FixedPoint, y: FixedPoint, radius: FixedPoint) {
   def rotate(rx: FixedPoint, ry: FixedPoint, angle: FixedPoint): Circle = {
     val dx = x - rx
     val dy = y - ry
-    val cos = FixedPoint.cos(angle)
-    val sin = FixedPoint.sin(angle)
-    val newdx = cos * dx - sin * dy
-    val newdy = sin * dx + cos * dy
+    val cosAngle = cos(angle)
+    val sinAngle = sin(angle)
+    val newdx = cosAngle * dx - sinAngle * dy
+    val newdy = sinAngle * dx + cosAngle * dy
     Circle(rx + newdx, ry + newdy, radius)
   }
 }
