ffcd42242a676e00d216362194ff2fae442cd9df
William Kretschmer <kretsch@mit.edu>
2016-12-02 13:49:37 -0500

    Computing cut sets as individual arcs

--- src/circlepuzzles/puzzle/Puzzle.scala
+++ src/circlepuzzles/puzzle/Puzzle.scala
@@ -14,17 +14,22 @@ class Puzzle(val moves: Set[Move]) {
     * A set of cuts. An entry `(circle, arcs)` such a map indicates that the arcs in `arcs` around the circle `circle`
     * belong in the cut set.
     */
-  type CutSet = mutable.Map[Circle, UnitArcs]
+  type CutSet = Map[Circle, UnitArcs]
 
   /**
-    * Add the cuts specified by the given `Circle` and `UnitArcs` to the given `CutSet`. This works by setting
+    * A mutable version of `CutSet`.
+    */
+  type MutableCutSet = mutable.Map[Circle, UnitArcs]
+
+  /**
+    * Add the cuts specified by the given `Circle` and `UnitArcs` to the given `MutableCutSet`. This works by setting
     * `cuts(circle)` equal to the union of `cuts(circle)` and `arcs`.
     * @param circle `Circle` to which the added cuts belong.
     * @param arcs `UnitArcs` describing the cuts around the given circle.
-    * @param cuts `CutSet` to which the cuts are to be added.
-    * @return `UnitArcs` that previously existed in the `CutSet` at the specified `Circle`.
+    * @param cuts `MutableCutSet` to which the cuts are to be added.
+    * @return `UnitArcs` that previously existed in the `MutableCutSet` at the specified `Circle`.
     */
-  private def add(circle: Circle, arcs: UnitArcs, cuts: CutSet): UnitArcs = {
+  private def add(circle: Circle, arcs: UnitArcs, cuts: MutableCutSet): UnitArcs = {
     cuts.get(circle) match {
       case None =>
         cuts.put(circle, arcs)
@@ -81,6 +86,30 @@ class Puzzle(val moves: Set[Move]) {
         myCuts.clear()
       }
     }
-    allCuts
+    allCuts.toMap
+  }
+
+  /**
+    * Computes the set of cuts in this puzzle as a list of individual arcs that start and end at intersections with
+    * other arcs.
+    * @return An iterable over all individual cut arcs in a full unbandaging of this puzzle.
+    */
+  def cutsAsArcs: Iterable[Arc] = {
+    val cuts = cutSet
+    val cutsByCircle = for((circle, arcs) <- cuts) yield {
+      // Compute the set of possible intersections with other circles as angles around this circle
+      val allIntersections = for((otherCircle, otherArcs) <- cuts ;
+                                 // Iterate through the 0, 1, or 2 intersections
+                                 (angle, otherAngle) <- circle.intersections(otherCircle)
+                                 // Only care about intersections that actually exist in the other circle
+                                 if otherArcs.contains(otherAngle))
+                             yield angle
+      // Above, we don't explicitly check if the intersections are contained in arcs
+      // This is because splitAtIntersections implicitly does this for us
+      val concreteArcs = arcs.splitAtIntersections(allIntersections.toList.distinct.sorted)
+      // Make an arc for each (start, end)
+      for((start, end) <- concreteArcs) yield Arc(circle, start, end)
+    }
+    cutsByCircle.flatten
   }
 }
