73a82770f55973c5592a0d6853982d03efddd0bc
William Kretschmer <kretsch@mit.edu>
2017-01-12 01:31:17 -0500

    Planar disk implementation

--- src/circlepuzzles/geometry/planar/Disk.scala
+++ src/circlepuzzles/geometry/planar/Disk.scala
@@ -2,12 +2,20 @@ package circlepuzzles.geometry.planar
 
 import circlepuzzles.geometry.{PlanarGeometry, Angle}
 
-class Disk extends PlanarGeometry.BaseDisk {
-  override def circle: Circle = ???
+/**
+  * Immutable disks in the Euclidean plane. A disk is uniquely defined as the interior of a circle.
+  * @param circle Boundary of this disk.
+  */
+case class Disk(circle: Circle) extends PlanarGeometry.BaseDisk {
+  override def rotate(rotationCenter: Point, angle: Angle): Disk = {
+    // Rotate the underlying circle
+    Disk(circle.rotate(rotationCenter, angle))
+  }
 
-  override def rotate(rotationCenter: Point, angle: Angle): Disk = ???
-
-  override def contains(pt: Point): Boolean = ???
-
-  override def strictlyContains(pt: Point): Boolean = ???
+  override def containsCompare(pt: Point): Int = {
+    // Compare the square of the distance to the center (i.e. dx^2 + dy^2) to the squared radius
+    val dx = circle.center.x - pt.x
+    val dy = circle.center.y - pt.y
+    (dx.pow(2) + dy.pow(2)).compare(circle.radius.pow(2))
+  }
 }
