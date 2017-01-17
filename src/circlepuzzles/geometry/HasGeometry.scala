496801fe6023613e40ce251759adfcf39576d22a
William Kretschmer <kretsch@mit.edu>
2017-01-17 02:23:39 -0500

    Refactor HasGeometry for better typing
    This works better because otherwise subclasses of HasPlanarGeometry
    don't know that geom has type PlanarGeometry.type. Using override val
    fixes this.

--- src/circlepuzzles/geometry/HasGeometry.scala
+++ src/circlepuzzles/geometry/HasGeometry.scala
@@ -3,11 +3,17 @@ package circlepuzzles.geometry
 /**
   * Classes that use a single geometry type for computations. Often, getting the particular types works by calling
   * `import geom._` at the top of a subclass.
-  * @param geom The particular geometry to use for computations.
   */
-abstract class HasGeometry(val geom: Geometry)
+abstract class HasGeometry {
+  /**
+    * The particular geometry to use for computations.
+    */
+  val geom: Geometry
+}
 
 /**
   * Classes that use Euclidean planar geometry.
   */
-abstract class HasPlanarGeometry extends HasGeometry(PlanarGeometry)
+trait HasPlanarGeometry extends HasGeometry {
+  override val geom = PlanarGeometry
+}
