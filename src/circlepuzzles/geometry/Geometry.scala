8ae4d0c47da63556f07b88808d5f5f01795cd937
William Kretschmer <kretsch@mit.edu>
2017-01-17 01:15:05 -0500

    Fix typo

--- src/circlepuzzles/geometry/Geometry.scala
+++ src/circlepuzzles/geometry/Geometry.scala
@@ -137,7 +137,7 @@ trait Geometry {
 
     /**
       * Compute the union of this with the given arcs by joining overlapping segments.
-      * @param that Arcs around a the same circle (i.e. requires `this.circle == that.circle`).
+      * @param that Arcs around the same circle (i.e. requires `this.circle == that.circle`).
       * @return `ArcsOnCircle` containing all arcs that belong to either `this` or `that`.
       * @throws IllegalArgumentException If `this.circle != that.circle`. Not every implementation checks this strictly.
       */
@@ -145,7 +145,7 @@ trait Geometry {
 
     /**
       * Compute the difference of this with the given arcs by subtracting overlapping segments.
-      * @param that Arcs around a the same circle (i.e. requires `this.circle == that.circle`).
+      * @param that Arcs around the same circle (i.e. requires `this.circle == that.circle`).
       * @return `ArcsOnCircle` containing all arcs that belong to `this` but not `that`.
       * @throws IllegalArgumentException If `this.circle != that.circle`. Not every implementation checks this strictly.
       */
