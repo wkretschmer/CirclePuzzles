38d725e1b98a240f869632f39c8f559588315fdf
William Kretschmer <kretsch@mit.edu>
2017-01-18 23:18:15 -0500

    Comments for spherical geometry

--- src/circlepuzzles/geometry/SphericalGeometry.scala
+++ src/circlepuzzles/geometry/SphericalGeometry.scala
@@ -1,5 +1,8 @@
 package circlepuzzles.geometry
 
+/**
+  * Geometry of the unit sphere.
+  */
 object SphericalGeometry extends Geometry {
   override type Point = spherical.Point
   override type Circle = spherical.Circle
