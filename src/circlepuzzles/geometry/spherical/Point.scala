83ef30a669ad8753af36f7f55fc221fe5fff58e9
William Kretschmer <kretsch@mit.edu>
2017-01-19 00:37:06 -0500

    Angles between points on the unit sphere
    This might get moved somewhere else, but the implementation is what's
    important.

--- src/circlepuzzles/geometry/spherical/Point.scala
+++ src/circlepuzzles/geometry/spherical/Point.scala
@@ -1,6 +1,6 @@
 package circlepuzzles.geometry.spherical
 
-import circlepuzzles.geometry.{Angle, SphericalGeometry}
+import circlepuzzles.geometry.{Angle, Vector3D, SphericalGeometry}
 import circlepuzzles.math.FixedPoint
 
 /**
@@ -32,5 +32,52 @@ case class Point(x: FixedPoint, y: FixedPoint, z: FixedPoint) extends SphericalG
     val newZ =
       (zxOneMinusCos - ySin) * x + (yzOneMinusCos + xSin) * y + (cos + rotationCenter.z.pow(2) * oneMinusCos) * z
     Point(newX, newY, newZ)
+  }
+
+  /**
+    * Returns a representation of this point as a vector in 3-space.
+    * @return A vector whose coordinates are the same as corresponding coordinates of this point.
+    */
+  def toVector3D: Vector3D = {
+    Vector3D(x, y, z)
+  }
+
+  /**
+    * Computes the angle of `end` from `start` in the counterclockwise direction about `this`. This can be viewed as the
+    * angle that `start` must be rotated counterclockwise about `this` for `this`, `start`, and `end` to all lie on a
+    * great circle. Therefore, the angle is signed in that `angle(start, end) + angle(end, start) = 2*pi` as a rule.
+    * Returns 0 in the degenerate cases where `this = start` or `this = end`.
+    * @param start Start point of the counterclockwise angle.
+    * @param end End point of the counterclockwise angle.
+    * @return Counterclockwise angle between `start` and `end` with respect to `this`.
+    */
+  def angle(start: Point, end: Point): Angle = {
+    // Vectors equivalent to this, start, and end, respectively
+    val this3D = toVector3D
+    val start3D = start.toVector3D
+    val end3D = end.toVector3D
+    // Projections of start and end, respectively, onto the plane orthogonal to this
+    // Subtract from the vectors their projection onto this, computed using formula proj_u(v) = u * ((v . u) / (u . u))
+    // These are points on the unit sphere, so u . u = 1 by assumption
+    val startProj = start3D - this3D * start3D.dotProduct(this3D)
+    val endProj = end3D - this3D * end3D.dotProduct(this3D)
+    // Angle between start and end, but in the range [0,pi]
+    val convexAngle = startProj.convexAngle(endProj)
+    // We use the right hand rule to figure out if we have the desired angle, or its explement (i.e. 2*pi - convexAngle)
+    // The cross product of the projected vectors will be in the same direction as this if we have the desired angle
+    val crossProduct = startProj.crossProduct(endProj)
+    // Test if the cross product is in the same direction as this (or 0) by looking comparing signs entrywise
+    val sameDirection = productIterator.zip(crossProduct.productIterator).forall {
+      case (thisCoord: FixedPoint, prodCoord: FixedPoint) =>
+        // Take the signs of corresponding coordinates
+        val thisSign = thisCoord.signum
+        val prodSign = prodCoord.signum
+        // Either all entries must have the same signs, or the product must be the 0 vector
+        prodSign == 0 || thisSign == prodSign
+    }
+    // Return the angle if the cross product is in the same direction
+    if(sameDirection) convexAngle
+    // Otherwise, return the explementary angle
+    else new Angle(FixedPoint.TwoPi - convexAngle.radians)
   }
 }
