4e9f90669874fb07d0a1474d99d20008ec435a45
William Kretschmer <kretsch@mit.edu>
2017-01-19 22:47:50 -0500

    Spherical circle and disk implementations
    Includes some work on ArcsOnCircle.

--- src/circlepuzzles/geometry/spherical/ArcsOnCircle.scala
+++ src/circlepuzzles/geometry/spherical/ArcsOnCircle.scala
@@ -1,11 +1,13 @@
 package circlepuzzles.geometry.spherical
 
 import circlepuzzles.geometry.{Angle, SphericalGeometry}
+import circlepuzzles.math.UnitArcs
 
-class ArcsOnCircle extends SphericalGeometry.BaseArcsOnCircle {
-  override def circle: Circle = ???
-
-  override def rotate(rotationCenter: Point, angle: Angle): ArcsOnCircle = ???
+class ArcsOnCircle(override val circle: Circle, val zero: Point, val unitArcs: UnitArcs) extends SphericalGeometry.BaseArcsOnCircle {
+  override def rotate(rotationCenter: Point, angle: Angle): ArcsOnCircle = {
+    // Rotate both the circle and the zero point, but not the unitArcs because they are with respect to the zero point
+    new ArcsOnCircle(circle.rotate(rotationCenter, angle), zero.rotate(rotationCenter, angle), unitArcs)
+  }
 
   override def intersection(disk: Disk): ArcsOnCircle = ???
 
@@ -13,5 +15,7 @@ class ArcsOnCircle extends SphericalGeometry.BaseArcsOnCircle {
 
   override def sameCircleUnion(that: ArcsOnCircle): ArcsOnCircle = ???
 
-  override def nonEmpty: Boolean = ???
+  override def nonEmpty: Boolean = {
+    unitArcs.nonEmpty
+  }
 }
--- src/circlepuzzles/geometry/spherical/Circle.scala
+++ src/circlepuzzles/geometry/spherical/Circle.scala
@@ -1,13 +1,64 @@
 package circlepuzzles.geometry.spherical
 
 import circlepuzzles.geometry.{Angle, SphericalGeometry}
+import circlepuzzles.math.{FixedPoint, UnitArcs}
 
-class Circle extends SphericalGeometry.BaseCircle {
-  override def center: Point = ???
+/**
+  * Circle on the unit sphere. A circle is defined as the set of points whose distance in radians to a given center
+  * point is equal to a given radius. Note that this representation is not unique; it is equivalent to a circle with
+  * opposite center and supplementary radius.
+  * @param center Center of this circle.
+  * @param radius Radius of this circle, in radians. Must be in the range (0,pi).
+  */
+class Circle(override val center: Point, val radius: Angle) extends SphericalGeometry.BaseCircle {
+  override def rotate(rotationCenter: Point, angle: Angle): Circle = {
+    // Rotate this by rotating the center, returning a circle with the same radius
+    new Circle(center.rotate(rotationCenter, angle), radius)
+  }
 
-  override def rotate(rotationCenter: Point, angle: Angle): Circle = ???
+  /**
+    * Returns a point on this circle.
+    * @return A point on this circle.
+    */
+  def arbitraryPoint: Point = {
+    // Compute the polar coordinates of the center
+    val (theta, phi) = center.toPolar
+    // Add the angle in the polar direction, and memoize so the constructor is more efficient
+    new Point(theta.memoized, (phi + radius).memoized)
+  }
 
-  override def emptyArcs: ArcsOnCircle = ???
+  override def emptyArcs: ArcsOnCircle = {
+    // Choose an arbitrary zero point
+    new ArcsOnCircle(this, arbitraryPoint, UnitArcs.Empty)
+  }
 
-  override def fullArcs: ArcsOnCircle = ???
+  override def fullArcs: ArcsOnCircle = {
+    // Choose an arbitrary zero point
+    new ArcsOnCircle(this, arbitraryPoint, UnitArcs.FullCircle)
+  }
+
+  override def equals(that: Any): Boolean = {
+    that match {
+      case thatCircle: Circle =>
+        // Circles can be equal in two cases:
+        // Either they have the same center and radii
+        if(center == thatCircle.center) {
+          radius == thatCircle.radius
+        }
+        // Or have opposite centers and supplementary radii
+        else if(-center == thatCircle.center) {
+          radius == thatCircle.radius.supplement
+        }
+        // Otherwise they are not equal
+        else false
+      case _ =>
+        false
+    }
+  }
+
+  override def hashCode: Int = {
+    // Because there are two distinct representations, we need to hash both of them
+    // TODO: is there a cheaper / more elegant way to do this? Does this hashCode work?
+    (center, radius).hashCode + (-center, radius.supplement).hashCode
+  }
 }
