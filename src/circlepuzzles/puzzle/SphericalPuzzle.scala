8b460bf00138eb1f0497cb1d4b5a5755810301c3
William Kretschmer <kretsch@mit.edu>
2017-01-20 23:31:09 -0500

    SphericalPuzzle flatten method
    Almost done with spherical puzzles!

--- src/circlepuzzles/puzzle/SphericalPuzzle.scala
+++ src/circlepuzzles/puzzle/SphericalPuzzle.scala
@@ -2,7 +2,7 @@ package circlepuzzles.puzzle
 
 import java.util
 
-import circlepuzzles.geometry.HasSphericalGeometry
+import circlepuzzles.geometry.{Angle, HasSphericalGeometry}
 
 /**
   * Circle puzzles on the unit sphere.
@@ -12,5 +12,30 @@ object SphericalPuzzle extends GeometricPuzzle with HasSphericalGeometry {
 
   override def emptySortedArcs(point: Point): util.TreeSet[(Arc, Boolean)] = ???
 
-  override def flatten(grouped: Iterable[ArcsOnCircle]): Iterable[Arc] = ???
+  override def flatten(grouped: Iterable[ArcsOnCircle]): Iterable[Arc] = {
+    val cutsByCircle = for(arcsOnCircle <- grouped) yield {
+      val circle = arcsOnCircle.circle
+      val zero = arcsOnCircle.zero
+      val unitArcs = arcsOnCircle.unitArcs
+      // Compute the set of possible intersections with other circles as angles around this circle
+      val allIntersectionPoints = for(other <- grouped ;
+                                 // Iterate through the 0, 1, or 2 intersections
+                                 intersection <- circle.intersections(other.circle)
+                                 // Only care about intersections that actually exist in the other circle
+                                 if other.unitArcs.contains(other.center.angle(other.zero, intersection).radians))
+        yield intersection
+      val allIntersectionAngles = allIntersectionPoints.toList.distinct.map(circle.center.angle(zero, _).radians)
+      // Above, we don't explicitly check if the intersections are contained in arcs
+      // This is because splitAtIntersections implicitly does this for us
+      val concreteArcs = unitArcs.splitAtIntersections(allIntersectionAngles.sorted)
+      // Make an arc for each (start, end)
+      for((start, end) <- concreteArcs) yield {
+        val center = circle.center
+        val startPoint = zero.rotate(center, new Angle(start))
+        val endPoint = zero.rotate(center, new Angle(end))
+        new Arc(center, startPoint, endPoint)
+      }
+    }
+    cutsByCircle.flatten
+  }
 }
